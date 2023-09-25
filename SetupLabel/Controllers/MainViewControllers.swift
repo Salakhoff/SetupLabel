069b77022c17361783af6daa1a7a906de8633d26
Ilfat <salakhoff.ilfat@gmail.com>
2023-09-25 18:40:59 +0300

    Установил кнопку

--- SetupLabel/Controllers/MainViewControllers.swift
+++ SetupLabel/Controllers/MainViewControllers.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Text"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var addButtonItem: UIBarButtonItem = {
+        var button = UIBarButtonItem()
+        button = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+    }
+    
+    @objc private func addButtonTapped() {
+        print("Add")
     }
 }
 
@@ -19,6 +37,21 @@ extension MainViewController {
     private func setupView() {
         title = "Текст"
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(label)
+        
+        navigationItem.rightBarButtonItem = addButtonItem
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.9)
+        ])
     }
 }
 
